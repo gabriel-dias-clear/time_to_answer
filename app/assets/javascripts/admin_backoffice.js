3d91806a1598d640851cd9bf78278db09511a418
gabriel-dias-clear <gabriel.dias.clear@gmail.com>
2023-10-19 11:44:32 -0300

    pagination kaminari

---
+++ app/assets/javascripts/admin_backoffice.js
@@ -0,0 +1,7 @@
+/* 
+*= require jquery/dist/jquery
+*= require metismenu/dist/metisMenu
+*= require bootstrap/dist/js/bootstrap
+*= require sb-admin-2
+*= require bootstrap-growl-ifightcrime/jquery.bootstrap-growl.js
+*/
